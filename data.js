8a290897900a3ac4a23ef653bcaa74c0ca36a210
Diptanup19 <82963285+Diptanup19@users.noreply.github.com>
2024-06-16 21:40:50 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,25 @@
-module.exports = {mongoLink:'mongodb+srv://dbuserdiptanu164:<yu-JJQAMKg-S65a>@cluster0.rb1bbfr.mongodb.net/',//database linkbot_token:'7232440588:AAEcLXmA5IgZIhAy4c1GzULYtNmPMhWJ2c8',bot_name:'Trial164Bot',//bot username without @bot_admin:1704662642,channelsList : ['WorldPayout'],channelscheck : ['@WorldPayout'],airdropName : 'Trial Bot',reffer_bonus:300,// ral bonus amountmin_wd:10,//min withdrawal amountcurrency:'trx', //bot cuurencypayment_channel:'@WorldPayout'// payment channel username}
+module.exports = {
+
+mongoLink:'mongodb+srv://dbuserdiptanu164:<yu-JJQAMKg-S65a>@cluster0.rb1bbfr.mongodb.net/',//database link
+
+bot_token:'7232440588:AAEcLXmA5IgZIhAy4c1GzULYtNmPMhWJ2c8',
+
+bot_name:'Trial164Bot',//bot username without @
+
+bot_admin:1704662642,
+
+channelsList : ['WorldPayout'],
+
+channelscheck : ['@WorldPayout'],
+
+airdropName : 'Trial Bot',
+
+reffer_bonus:300,// ral bonus amount
+
+min_wd:10,//min withdrawal amount
+
+currency:'trx', //bot cuurency
+
+payment_channel:'@WorldPayout'// payment channel username
+
+}
